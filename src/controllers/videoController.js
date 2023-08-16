c2e604ef80cea86d4432782b0f8231136f9dbd78
Jaewon Han <jaewonhan20@gmail.com>
2023-08-16 00:10:23 -0500

    MongoDB, POST

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,43 @@
-export const trending=(req,res)=>res.render("home");
-export const see=(req,res)=>res.render("watch");
-export const edit=(req,res)=>res.render("edit");
-export const search=(req,res)=>res.send("Search");
-export const upload=(req,res)=>res.send("Upload");
-export const deleteVideo=(req,res)=>res.send("Delete");
+import Video from "../models/Video";
 
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: "Editing" });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+};
+export const search = (req, res) => {
+  return res.render("search", { pageTitle: "Search", results: [] });
+};
