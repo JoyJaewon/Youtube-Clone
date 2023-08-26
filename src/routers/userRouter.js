4fd117e24eb97403293d24ce11719918d599e721
Jaewon Han <jaewonhan20@gmail.com>
2023-08-26 13:17:03 -0500

    Add change password functionality and views

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,6 +5,8 @@ import {
   logout,
   remove,
   see,
+  getChangePassword,
+  postChangePassword,
 } from "../controllers/userController";
 import { protectorMiddleware } from "../middlewares";
 
@@ -12,6 +14,11 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.get("/delete", remove);
 userRouter.get(":id", see);
 
